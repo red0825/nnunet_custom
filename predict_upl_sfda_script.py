93e2f7f62ebffe83b9509d19e1f31631acf79724
Hongbin Jeong <65748633+red0825@users.noreply.github.com>
2025-03-03 15:58:40 +0900

    Update and rename predict_upl_sfda_script.py to predict_sfda_script.py

--- predict_upl_sfda_script.py
+++
@@ -1,7 +0,0 @@
-import sys
-import multiprocessing
-from nnunetv2.inference.predict_from_raw_data import predict_entry_point_upl_sfda
-
-if __name__ == '__main__':
-    multiprocessing.freeze_support()
-    predict_entry_point_upl_sfda()
